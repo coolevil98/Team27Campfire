0c341e4a5bc65f59270629e9e7c029eca4664a54
coolevil98 <odinbraad@gmail.com>
2023-03-24 10:27:08 +1100

    Begun to structure timer

--- Assets/Scripts/IntroText.cs
+++ Assets/Scripts/IntroText.cs
@@ -10,16 +10,23 @@ public class IntroText : MonoBehaviour
     #region Variables
     public TMP_Text introText;
     public List<string> introTextCollection = new List<string>();
+    public float textDisplayTime;
+    private float storeTime;
     #endregion
     // Start is called before the first frame update
     void Start()
     {
-        
+        storeTime = textDisplayTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        storeTime-=Time.deltaTime;
+        if(storeTime <=- 0)
+        {
+            //change text go up one for the list
+            //if list reaches end then after one more timer destory the canvas to reduce load
+        }
     }
 }
