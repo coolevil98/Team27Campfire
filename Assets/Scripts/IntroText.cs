bc2a60cd38c9ea9b263528eb0c32a4f4bf2acf3c
coolevil98 <odinbraad@gmail.com>
2023-03-23 17:36:17 +1100

    Begun to set up the intro text manager

---
+++ Assets/Scripts/IntroText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Linq;
+
+public class IntroText : MonoBehaviour
+{
+    #region Variables
+    public TMP_Text introText;
+    public List<string> introTextCollection = new List<string>();
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
