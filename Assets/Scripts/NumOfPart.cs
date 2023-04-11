4ef8f897231b1047ec54916fe687a54791827e0b
SpotTheDog <61957410+SpotTheDog@users.noreply.github.com>
2023-04-11 16:04:42 +1000

    change the emission over time

---
+++ Assets/Scripts/NumOfPart.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumOfPart : MonoBehaviour
+{
+    public ParticleSystem fireParticles;
+    private float timer = 0f;
+    public float emRate;
+    public float timeToChangeInSec;
+    public float newRate1;
+    public float newRate2;
+    public float newRate3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(fireParticlesEmission());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        var emission = fireParticles.emission;
+        emission.rateOverTime = emRate;
+    }
+
+    IEnumerator fireParticlesEmission()
+    {
+        //the 60 is for the intro
+        yield return new WaitForSeconds(60 + timeToChangeInSec);
+        emRate = newRate1;
+        Debug.Log("changed");
+        yield return new WaitForSeconds(timeToChangeInSec);
+        emRate = newRate2;
+        Debug.Log("changed2");
+        yield return new WaitForSeconds(timeToChangeInSec);
+        emRate = newRate3;
+        Debug.Log("changed3");
+    }
+}
