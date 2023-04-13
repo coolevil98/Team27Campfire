d686822dca911131de4e4805894f023a6d2f955a
BuildTools <knikyle@gmail.com>
2023-04-13 16:49:29 +1000

    added a fast forward script
    bound to w or right controller axis (while the joystick is held down)

---
+++ Assets/Scripts/FastForward.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Liminal.SDK.VR;
+using Liminal.SDK.VR.Input;
+
+public class FastForward : MonoBehaviour
+{
+    public float ffSpeed = 19;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (VRDevice.Device.PrimaryInputDevice.GetButton(VRButton.Seconday))
+        {
+            Time.timeScale = 1 + (ffSpeed * VRDevice.Device.PrimaryInputDevice.GetAxis2D(VRAxis.One).y);
+        }
+        else
+        {
+            Time.timeScale = 1 + (ffSpeed * Input.GetAxis("Vertical"));
+        }
+        
+    }
+}
