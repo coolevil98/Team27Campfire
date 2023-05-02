201b4bb5858481952db5ce2a63e37e207bb4ed8b
coolevil98 <odinbraad@gmail.com>
2023-05-02 16:18:17 +1000

    Created blackout effect

---
+++ Assets/Scripts/BlackoutEffect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackoutEffect : MonoBehaviour
+{
+    public GameObject wall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        wall.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            wall.SetActive(true);
+            StartCoroutine(makeInvisible());
+        }
+    }
+
+    IEnumerator makeInvisible()
+    {
+        yield return new WaitForSeconds(2);
+        wall.SetActive(false);
+    }
+}
