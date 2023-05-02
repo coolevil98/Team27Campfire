c9840972d1dd8418b2df42ed1ad22352a1579418
coolevil98 <odinbraad@gmail.com>
2023-05-02 16:37:23 +1000

    In progress blackline going across screen

---
+++ Assets/Scripts/BlackoutMoving.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class BlackoutMoving : MonoBehaviour
+{
+    public GameObject movingWall;
+    //this will move the panning object across the screen
+    //such as from right to left
+    //could use transform translate
+    //on start put it back to the beginning position
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            movingWall.transform.position= Vector3.Lerp(new Vector3(10,0,3), new Vector3(-10, 0, 3), 0.2f);
+            Debug.Log("hi");
+        }
+    }
+}
