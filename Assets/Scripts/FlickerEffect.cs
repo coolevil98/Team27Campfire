e84ba41601d2ba1ce6a173d5f999aec3a3fc6fe1
BuildTools <knikyle@gmail.com>
2023-05-24 14:12:51 +1000

    hopefully flicker is good now
    edited speeds and make the flicker fade

--- Assets/Scripts/FlickerEffect.cs
+++ Assets/Scripts/FlickerEffect.cs
@@ -8,10 +8,17 @@ public class FlickerEffect : MonoBehaviour
     private float timer;
     private float pingPongTimer;
     public GameObject ball;
+    private Color ballColor;
+    private Renderer ballRenderer;
     public float speedMult = 1;
     public float speed = 1;
     private bool turnOn = false;
     public float degree;
+
+    public AnimationCurve ballFade;
+    public float ballCycle = 1; //seconds for ball to complete full transperency cycle
+    private float aTimer;
+    private float aIncrement;
 
     public Vector3[] speedMults = new Vector3[2];//x for min speed, y for max speed, z for time
     public int randTimer = 3; //number of seconds to calculate a random speed
@@ -25,9 +32,11 @@ public class FlickerEffect : MonoBehaviour
     void Start()
     {
         ball.SetActive(false);
-
+        ballRenderer = ball.GetComponent<Renderer>();
+        ballColor = ballRenderer.material.color;
         timeDif = speedMults[speedsIndex + 1].z - speedMults[speedsIndex].z;
         speedsIndex++;
+        aIncrement = 1 / (25 * ballCycle);
     }
 
     // Update is called once per frame
@@ -53,6 +62,8 @@ public class FlickerEffect : MonoBehaviour
     void FixedUpdate()
     {
         pingPongTimer += speed * speedMult;
+        print(ballColor);
+        BallFlash();
     }
 
     public void RotateCircle()
@@ -76,8 +87,16 @@ public class FlickerEffect : MonoBehaviour
         if (timer / randTimer > randTimes)
         {
             randTimes++;
-            speedMult = Random.Range(minSpeedMult, maxSpeedMult);
-            print(speedMult);
+            speedMult = Random.Range(minSpeedMult, maxSpeedMult + 2); //+2 to make it more likely to be at higher speed
+            speedMult = Mathf.Clamp(speedMult, minSpeedMult, maxSpeedMult);
         }
+    }
+
+    public void BallFlash()
+    {
+        aTimer += aIncrement;
+        ballColor.a = ballFade.Evaluate(aTimer);
+        print(aTimer);
+        ballRenderer.material.color = ballColor;
     }
 }
