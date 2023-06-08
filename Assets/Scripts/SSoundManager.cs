184f42f8debef3ccdc9e49d40cf4f5168ade7c79
IsaacJ46591168 <isaac.james@students.mq.edu.au>
2023-06-08 13:48:34 +1000

    Audio additions
    added more audio of people talking and made some adjustments to volume for some others

--- Assets/Scripts/SSoundManager.cs
+++ Assets/Scripts/SSoundManager.cs
@@ -2,7 +2,7 @@
 using System;
 using UnityEngine;
 using System.Collections;
-public class SSoundManager : MonoBehaviour
+public class  : MonoBehaviour
 {
     public SecondarySound[] sounds;
     private bool stopIntro;
@@ -26,6 +26,11 @@ public class SSoundManager : MonoBehaviour
         Play("windintro");
         Play("introchatter");
         Play("introchatter2");
+        Play("randomtalk1");
+        Play("randomtalk2");
+        Play("randomtalk4");
+        Play("randomtalk8");
+        Play("randomtalk9");
         stopIntro = true;
     }
 
@@ -46,133 +51,150 @@ public class SSoundManager : MonoBehaviour
         //system for determining what sound effect plays where. Once all the sound effects are at the right volume/right order, I might get rid of this and put all the sound effects
         //into one continuous audio file so we don't have to have 1 billion if statements
         if(fireTimer.getTimer >= 5.0f && fireTimer.getTimer <= 5.5f){
-            FindObjectOfType<SSoundManager>().Play("Narration1");
+            Play("Narration1");
         }
 
         if(fireTimer.getTimer >= 10.0f && fireTimer.getTimer <= 10.5f){
-            FindObjectOfType<SSoundManager>().Play("bottledrink");
+            Play("bottledrink");
         }
 
         if(fireTimer.getTimer >= 12.0f && fireTimer.getTimer <= 12.5f){
-            FindObjectOfType<SSoundManager>().Play("chairshuffle1");
+            Play("chairshuffle1");
         }
 
         if(fireTimer.getTimer >= 17.0f && fireTimer.getTimer <= 17.5f){
-            FindObjectOfType<SSoundManager>().Play("Narration2");
+            Play("Narration2");
         }
 
         if(fireTimer.getTimer >= 30.0f && fireTimer.getTimer <= 30.5f){
-            FindObjectOfType<SSoundManager>().Play("crickets");
+            Play("crickets");
+            Play("randomtalk10");
+
+        }
+
+        if(fireTimer.getTimer >= 35.0f && fireTimer.getTimer <= 35.5f){
+            Play("randomtalk3");
+        }
+
+        if(fireTimer.getTimer >= 40.0f && fireTimer.getTimer <= 40.5f){
+            Play("randomtalk5");
         }
 
         if(fireTimer.getTimer >= 55.0f && fireTimer.getTimer <= 55.5f){
-            FindObjectOfType<SSoundManager>().Play("murmur");
+            Play("murmur");
+            Play("randomtalk11");
         }
 
         if(fireTimer.getTimer >= 75.0f && fireTimer.getTimer <= 75.5f){
-            FindObjectOfType<SSoundManager>().Play("chairshuffle2");
+            Play("chairshuffle2");
         }
 
         if(fireTimer.getTimer >= 90.0f && fireTimer.getTimer <= 90.5f){
-            FindObjectOfType<SSoundManager>().Play("chairshuffle3");
+            Play("chairshuffle3");
+            Play("randomtalk6");
+
+        }
+
+        if(fireTimer.getTimer >= 110.0f && fireTimer.getTimer <= 110.5f){
+            Play("randomtalk7");
         }
 
         if(fireTimer.getTimer >= 180.0f && fireTimer.getTimer <= 180.5f){
-            FindObjectOfType<SSoundManager>().Play("murmur2");
+            Play("murmur2");
         }
 
         if(fireTimer.getTimer >= 200.0f && fireTimer.getTimer <= 200.5f){
-            FindObjectOfType<SSoundManager>().Play("bottledrink2");
+            Play("bottledrink2");
         }
 
         if(fireTimer.getTimer >= 222.0f && fireTimer.getTimer <= 222.5f){
-            FindObjectOfType<SSoundManager>().Play("baglook");
+            Play("baglook");
         }
 
         if(fireTimer.getTimer >= 235.0f && fireTimer.getTimer <= 235.5f){
-            FindObjectOfType<SSoundManager>().Play("cupfiddle");
+            Play("cupfiddle");
         }
 
         if(fireTimer.getTimer >= 250.0f && fireTimer.getTimer <= 250.5f){
-            FindObjectOfType<SSoundManager>().Play("murmur3");
+            Play("murmur3");
         }
 
         if(fireTimer.getTimer >= 285.0f && fireTimer.getTimer <= 285.5f){
-            FindObjectOfType<SSoundManager>().Play("murmur4");
+            Play("murmur4");
         }
 
         if(fireTimer.getTimer >= 320.0f && fireTimer.getTimer <= 320.5f){
-            FindObjectOfType<SSoundManager>().Play("Narration3");
+            Play("Narration3");
         }
 
         if(fireTimer.getTimer >= 350.0f && fireTimer.getTimer <= 350.5f){
-            FindObjectOfType<SSoundManager>().Play("tentzip");
+            Play("tentzip");
         }
 
         if(fireTimer.getTimer >= 380.0f && fireTimer.getTimer <= 380.5f){
-            FindObjectOfType<SSoundManager>().Play("crickets");
-            FindObjectOfType<SSoundManager>().Play("sleepingbagroll");
+            Play("crickets");
+            Play("sleepingbagroll");
         }
 
         if(fireTimer.getTimer >= 410.0f && fireTimer.getTimer <= 410.5f){
-            FindObjectOfType<SSoundManager>().Play("sigh 1");
+            Play("sigh 1");
         }
 
         if(fireTimer.getTimer >= 415.0f && fireTimer.getTimer <= 415.5f){
-            FindObjectOfType<SSoundManager>().Play("Narration4");
+            Play("Narration4");
         }
 
         if(fireTimer.getTimer >= 440.0f && fireTimer.getTimer <= 440.5f){
-            FindObjectOfType<SSoundManager>().Play("sleepingbagroll2");
+            Play("sleepingbagroll2");
         }
 
         if(fireTimer.getTimer >= 450.0f && fireTimer.getTimer <= 450.5f){
-            FindObjectOfType<SSoundManager>().Play("murmur5");
-        } //turn down slightly
+            Play("murmur5");
+        }
 
         if(fireTimer.getTimer >= 470.0f && fireTimer.getTimer <= 470.5f){
-            FindObjectOfType<SSoundManager>().Play("sleepingbagroll3");
+            Play("sleepingbagroll3");
         }
 
         if(fireTimer.getTimer >= 485.0f && fireTimer.getTimer <= 485.5f){
-            FindObjectOfType<SSoundManager>().Play("Narration5");
+            Play("Narration5");
         }
 
         if(fireTimer.getTimer >= 510.0f && fireTimer.getTimer <= 510.5f){
-            FindObjectOfType<SSoundManager>().Play("tentzip2");
-        } //turn down to same as tentzip 1
+            Play("tentzip2");
+        }
 
         if(fireTimer.getTimer >= 520.0f && fireTimer.getTimer <= 520.5f){
-            FindObjectOfType<SSoundManager>().Play("bottledrink3");
-        } //turn down
+            Play("bottledrink3");
+        }
 
         if(fireTimer.getTimer >= 527.0f && fireTimer.getTimer <= 527.5f){
-            FindObjectOfType<SSoundManager>().Play("sigh2");
-        } //turn down
+            Play("sigh2");
+        }
 
         if(fireTimer.getTimer >= 540.0f && fireTimer.getTimer <= 540.5f){
-            FindObjectOfType<SSoundManager>().Play("cupfiddle3");
+            Play("cupfiddle3");
         }
 
         if(fireTimer.getTimer >= 550.0f && fireTimer.getTimer <= 550.5f){
-            FindObjectOfType<SSoundManager>().Play("sleepingbagroll4");
+            Play("sleepingbagroll4");
         }
 
         if(fireTimer.getTimer >= 555.0f && fireTimer.getTimer <= 550.5f){
-            FindObjectOfType<SSoundManager>().Play("sigh3");
+            Play("sigh3");
         }
 
         if(fireTimer.getTimer >= 560.0f && fireTimer.getTimer <= 560.5f){
-            FindObjectOfType<SSoundManager>().Play("sleepingbagroll5");
+            Play("sleepingbagroll5");
         }
 
         if(fireTimer.getTimer >= 575.0f && fireTimer.getTimer <= 575.5f){
-            FindObjectOfType<SSoundManager>().Play("Narration6");
+            Play("Narration6");
         }
 
         if(fireTimer.getTimer >= 600.0f && fireTimer.getTimer <= 600.5f){
-            FindObjectOfType<SSoundManager>().Play("tentzip3");
-            FindObjectOfType<SSoundManager>().Play("crickets");
+            Play("tentzip3");
+            Play("crickets");
         }
     }
 
@@ -213,15 +235,5 @@ public class SSoundManager : MonoBehaviour
             yield return new WaitForSeconds(0.1f);
         }
     }
-
-    // public void VolumeMultiplier(string name, float value)
-    // {
-    //     SecondarySound s = Array.Find(sounds, sound => sound.name == name);
-    //     if(s == null){
-    //         return;
-    //     }
-
-    //     s.source.volume *= value;
-    // }
 
 }
